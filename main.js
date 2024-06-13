e0d39fd9d3d3368f732da0ac099084707976fcfd
Andr3yvt <132575177+Andr3yvt@users.noreply.github.com>
2024-06-13 09:43:19 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -136,3 +136,20 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(element);
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const acc = document.querySelectorAll('.accordion');
+    
+    acc.forEach(button => {
+        button.addEventListener('click', function() {
+            this.classList.toggle('active');
+            
+            const panel = this.nextElementSibling;
+            if (panel.style.display === 'block') {
+                panel.style.display = 'none';
+            } else {
+                panel.style.display = 'block';
+            }
+        });
+    });
+});
