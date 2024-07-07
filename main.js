3cc93a8dae4856b7fda32122473a019013a0a14a
Andr3yvt <132575177+Andr3yvt@users.noreply.github.com>
2024-07-07 17:14:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,86 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Preloader
+    window.addEventListener('load', function() {
+        document.body.classList.add('no-scroll');
+        setTimeout(function() {
+            document.getElementById('preloader').style.opacity = '0';
+            document.getElementById('main-content').classList.remove('hide');
+            document.getElementById('main-content').classList.add('show');
+            setTimeout(function() {
+                document.getElementById('preloader').style.display = 'none';
+                document.body.classList.remove('no-scroll');
+            }, 1000);
+        }, 3000);
+    });
+
+ // Login Modal
+const loginBtn = document.getElementById('login-btn');
+const loginModal = document.getElementById('login-modal');
+const closeBtn = document.querySelector('.button');
+const loginForm = document.getElementById('login-form');
+const loggedInUsername = localStorage.getItem('loggedInUsername');
+
+const openLoginModal = () => {
+    loginModal.style.display = 'block';
+};
+
+const closeLoginModal = () => {
+    loginModal.style.display = 'none';
+};
+
+loginBtn.addEventListener('click', () => {
+    openLoginModal();
+    closeSettingsModal(); // Fecha o modal de configurações se estiver aberto
+});
+closeBtn.addEventListener('click', closeLoginModal);
+
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    loginBtn.style.display = 'none';
+    closeLoginModal();
+    document.getElementById('logged-in-user').textContent = usernameInput.value;
+    document.getElementById('logged-in-user').style.display = 'block';
+    localStorage.setItem('loggedInUsername', usernameInput.value);
+});
+
+// Função para abrir e fechar o modal de configurações
+document.getElementById('settings-btn').addEventListener('click', function() {
+    var modal = document.getElementById('settings-modal');
+    if (modal.style.display === 'none' || modal.style.display === '') {
+        modal.style.display = 'block';
+        closeLoginModal(); // Fecha o modal de login se estiver aberto
+    } else {
+        modal.classList.add('fade-out');
+        setTimeout(() => {
+            modal.style.display = 'none';
+            modal.classList.remove('fade-out');
+        }, 300); // Tempo da animação de fade-out (0.3s)
+    }
+});
+
+// Função para fechar o modal de configurações
+function closeSettingsModal() {
+    var modal = document.getElementById('settings-modal');
+    if (modal.style.display !== 'none') {
+        modal.classList.add('fade-out');
+        setTimeout(() => {
+            modal.style.display = 'none';
+            modal.classList.remove('fade-out');
+        }, 300); // Tempo da animação de fade-out (0.3s)
+    }
+}
+
+
+    // Seções Toggle
     const buttons = document.querySelectorAll('.toggle-section');
-    const sections = document.querySelectorAll('main > section'); // Seleciona todas as seções dentro do main
-    const servicosSection = document.getElementById('servicos'); // Obtém a seção "Nossos Serviços"
-    const tecnologiaSection = document.getElementById('tecnologia'); // Obtém a seção "Tecnologia Avançada"
-    const sustentabilidadeSection = document.getElementById('sustentabilidade'); // Obtém a seção "Sustentabilidade"
+    const sections = document.querySelectorAll('main > section');
+    const servicosSection = document.getElementById('servicos');
+    const tecnologiaSection = document.getElementById('tecnologia');
+    const sustentabilidadeSection = document.getElementById('sustentabilidade');
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            const targetSectionId = button.getAttribute('data-target').substring(1); // Obtém o ID da seção alvo sem o '#'
+            const targetSectionId = button.getAttribute('data-target').substring(1);
             sections.forEach(section => {
                 if (section.id !== targetSectionId && section.id !== 'servicos' && section.id !== 'tecnologia'
                  && section.id !== 'sustentabilidade') {
@@ -24,6 +97,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    // Formulário de Cultivo
     const form = document.getElementById('cultivo-form');
     const listaCultivos = document.getElementById('lista-cultivos');
 
@@ -32,7 +106,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const tipo = document.getElementById('tipo').value;
         const dataPlantio = document.getElementById('data-plantio').value;
         const area = document.getElementById('area').value;
-        
+
         const listItem = document.createElement('li');
         listItem.textContent = `Tipo: ${tipo}, Data de Plantio: ${dataPlantio}, Área: ${area} hectares`;
         listaCultivos.appendChild(listItem);
@@ -40,6 +114,7 @@ document.addEventListener('DOMContentLoaded', () => {
         form.reset();
     });
 
+    // Clima
     const apiKey = 'ac3976b1ab5f5c70fec9cd0a2b2bb9df';
     const searchButton = document.getElementById('search');
     const cityInput = document.getElementById('city-input');
@@ -70,49 +145,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 .catch(error => console.error('Error fetching weather data:', error));
         }
     });
-});
 
-document.addEventListener('DOMContentLoaded', () => {
-    const loginBtn = document.getElementById('login-btn');
-    const loginModal = document.getElementById('login-modal');
-    const closeBtn = document.querySelector('.button');
-    const loginForm = document.getElementById('login-form');
-    const usernameInput = document.getElementById('username');
-    const passwordInput = document.getElementById('password');
-    const loggedInUsername = localStorage.getItem('loggedInUsername');
-
-    // Função para exibir o modal de login
-    const openLoginModal = () => {
-        loginModal.style.display = 'block';
-    };
-
-    // Função para fechar o modal de login
-    const closeLoginModal = () => {
-        loginModal.style.display = 'none';
-    };
-
-    // Evento de clique no botão de login
-    loginBtn.addEventListener('click', openLoginModal);
-
-    // Evento de clique no botão de fechar
-    closeBtn.addEventListener('click', closeLoginModal);
-
-    // Evento de envio do formulário de login
-    loginForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-
-        // Oculta o botão de login
-        loginBtn.style.display = 'none';
-        closeLoginModal(); // Fecha o modal de login
-        document.getElementById('logged-in-user').textContent = usernameInput.value;
-        document.getElementById('logged-in-user').style.display = 'block';
-        localStorage.setItem('loggedInUsername', usernameInput.value);
-    });
-});
-
-
-// script.js
-document.addEventListener('DOMContentLoaded', () => {
+    // Intersection Observer
     const quemSomosElements = document.querySelectorAll('.quem-somos-content div');
     const servicosElements = document.querySelectorAll('.servico');
     const tecnologiaElements = document.querySelectorAll('.Tecnologia');
@@ -130,7 +164,7 @@ document.addEventListener('DOMContentLoaded', () => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('visible');
-                observer.unobserve(entry.target); // Stop observing once the item is visible
+                observer.unobserve(entry.target);
             }
         });
     };
@@ -140,16 +174,14 @@ document.addEventListener('DOMContentLoaded', () => {
     elements.forEach(element => {
         observer.observe(element);
     });
-});
 
-document.addEventListener('DOMContentLoaded', () => {
-    const acc = document.querySelectorAll('.accordion');
-    
-    acc.forEach(button => {
-        button.addEventListener('click', function() {
-            this.classList.toggle('active');
-            
-            const panel = this.nextElementSibling;
+    // Accordion
+    const accordions = document.querySelectorAll('.accordion');
+
+    accordions.forEach(accordion => {
+        accordion.addEventListener('click', () => {
+            const panel = accordion.nextElementSibling;
+            accordion.classList.toggle('active');
             if (panel.style.display === 'block') {
                 panel.style.display = 'none';
             } else {
@@ -157,24 +189,4 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
-});
-
-window.addEventListener('load', function() {
-    // Desativa o scroll
-    document.body.classList.add('no-scroll');
-    
-    // Aguarda 3 segundos antes de iniciar a transição
-    setTimeout(function() {
-        // Inicia a transição do preloader para o conteúdo principal
-        document.getElementById('preloader').style.opacity = '0';
-        document.getElementById('main-content').classList.remove('hide');
-        document.getElementById('main-content').classList.add('show');
-        
-        // Aguarda a duração da transição antes de remover o preloader
-        setTimeout(function() {
-            document.getElementById('preloader').style.display = 'none';
-            // Ativa o scroll novamente
-            document.body.classList.remove('no-scroll');
-        }, 1000); // 1 segundo para coincidir com a duração da transição no CSS
-    }, 3000);
 });
